e407718c4f70f9f73b153f463a77074700d3c81e
rafaelpborba1 <rpb@cesar.school>
2026-09-08 18:27:20 -0300

    adiciona validacao de argumentos na CLI

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void) {
-    printf("Compilacao bem-sucedida!\n");
-    return 0;
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        fprintf(stderr, "Uso incorreto. Sintaxe: %s <RM|EDF> <arquivo_entrada>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    char *algoritmo = argv[1];
+    char *arquivo_entrada = argv[2];
+
+    if (strcmp(algoritmo, "RM") != 0 && strcmp(algoritmo, "EDF") != 0) {
+        fprintf(stderr, "Erro: Algoritmo '%s' invalido. Escolha 'RM' ou 'EDF'.\n", algoritmo);
+        return EXIT_FAILURE;
+    }
+
+    printf("Algoritmo: %s | Arquivo: %s\n", algoritmo, arquivo_entrada);
+
+    return EXIT_SUCCESS;
 }
-
